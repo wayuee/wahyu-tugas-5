44433d0b02bb425b19e56210960f35b1efc22905
wayuee <farhancoc5@gmail.com>
2021-12-21 22:22:54 +0700

    ganti ke array

--- components/DataFromIndex.vue
+++ components/DataFromIndex.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img class="rounded-full float-left mx-5 mt-5" :src="dataTeam.img" alt="bebas"/>
+    <img class="rounded-full float-left mx-5 mt-1" :src="dataTeam.img" alt="bebas"/>
     <h4 class="z-20 font-semibold text-white mt-5">{{ dataTeam.name }}</h4>
     <p class="z-20 text-gray-400 mb-10">{{ dataTeam.graduate }}</p>
     <h3 class="z-20 font-bold -mt-5 px-5 text-white relative">{{ dataTeam.bio }}</h3>

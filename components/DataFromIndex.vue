34120994f0b678cf6a6f586e5bb6e89f9de75bbf
wayuee <farhancoc5@gmail.com>
2021-12-21 22:14:56 +0700

    ganti ke array

--- components/DataFromIndex.vue
+++ components/DataFromIndex.vue
@@ -1,84 +1,10 @@
 <template>
-  <div class="flex justify-content item-center min-h-screen bg-gray-800">
-    <div class="grid grid-cols-4 gap-4 py-20 px-52">
-      <div
-        class="col-start-1 col-span-2 rounded-xl border-solid border-2 black bg-blue-900"
-      >
-        <img
-          class="rounded-full float-left mx-5 mt-5"
-          :src="dataTeam.imgDaniel"
-          alt="Daniel"
-        />
-        <h4 class="z-20 font-semibold text-white mt-5">
-          {{ dataTeam.nameDaniel }}
-        </h4>
-        <p class="z-20 text-gray-400">{{ dataTeam.graduateDaniel }}</p>
-        <svg
-          class="ml-96 -mt-14"
-          width="120"
-          height="100"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M104 102V59.727H84.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42   -9.287 6.125-13.61C95.01 25.86 98.909 22.257 104 19.375V0c-9.758 4.27-17.712 9.874-23.864 16.813-6.151 6.939-10.712 14.545-13.681 22.818C63.485 47.904 62 59.941 62 75.74V102h42zm-62 0V59.727H22.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42-9.287 6.125-13.61C33.01 25.86 36.909 22.257 42 19.375V0c-9.652 4.27-17.58 9.874-23.784 16.813C12.01 23.752 7.424 31.358 4.455 39.631 1.485 47.904 0 59.941 0 75.74V102h42z"
-            fill="#A775F1"
-            fill-rule="nonzero"
-          />
-        </svg>
-        <h3 class="z-20 font-bold -mt-5 px-5 text-white">
-          {{ dataTeam.bioDaniel }}
-        </h3>
-        <p class="z-20 px-7 pt-3 text-gray-300">{{ dataTeam.textDaniel }}</p>
-      </div>
-      <div class="rounded-xl border-solid border-2 black bg-gray-800">
-        <img
-          class="rounded-full float-left mt-5 mx-5"
-          :src="dataTeam.imgJonathan"
-          alt="Jonathan"
-        />
-        <h4 class="font-semibold text-white mt-5">{{ dataTeam.nameJonathan }}</h4>
-        <p class="text-gray-400 pb-5">{{ dataTeam.graduateJonathan }}</p>
-        <h3 class="font-bold px-3 pb-3 text-white">{{ dataTeam.bioJonathan }}</h3>
-        <p class="px-5 text-gray-300">{{ dataTeam.textJonathan }}</p>
-      </div>
-      <div
-        class="row-span-2 rounded-xl border-solid border-2 black bg-gray-500"
-      >
-        <img
-          class="rounded-full float-left mt-5 mx-5"
-          :src="dataTeam.imgKira"
-          alt="Kira"
-        />
-        <h4 class="font-semibold text-white mt-5">{{ dataTeam.nameKira }}</h4>
-        <p class="text-gray-400 pb-5">{{ dataTeam.graduateKira }}</p>
-        <h3 class="font-bold px-3 pb-3">{{ dataTeam.bioKira }}</h3>
-        <p class="px-3 pb-3 text-gray-300">{{ dataTeam.textKira }}</p>
-      </div>
-      <div class="rounded-xl border-solid border-2 black bg-gray-500">
-        <img
-          class="rounded-full float-left mt-5 mx-5"
-          :src="dataTeam.imgJeanette"
-          alt="jeanette"
-        />
-        <h4 class="font-semibold text-white mt-5">{{ dataTeam.nameJeanette }}</h4>
-        <p class="text-gray-400 pb-5">{{ dataTeam.graduateJeanette }}</p>
-        <h3 class="font-bold px-3">{{ dataTeam.bioJeanette }}</h3>
-        <p class="px-5 pt-3 text-gray-300">{{ dataTeam.textJeanette }}</p>
-      </div>
-      <div
-        class="col-start-2 col-span-2 rounded-xl border-solid border-2 black bg-gray-800"
-      >
-        <img
-          class="col-span-2 rounded-full float-left mt-5 mx-5"
-          :src="dataTeam.imgPatrick"
-          alt="Patrick"
-        />
-        <h4 class="font-semibold text-white mt-5">{{ dataTeam.namePatrick }}</h4>
-        <p class="text-gray-400 pb-5">{{ dataTeam.graduatePatrick }}</p>
-        <h3 class="font-bold px-5 pb-3 text-white">{{ dataTeam.bioPatrick }}</h3>
-        <p class="px-5 text-gray-300">{{ dataTeam.textPatrick }}</p>
-      </div>
-    </div>
+  <div>
+    <img class="rounded-full float-left mx-5 mt-5" :src="dataTeam.img" alt="bebas"/>
+    <h4 class="z-20 font-semibold text-white mt-5">{{ dataTeam.name }}</h4>
+    <p class="z-20 text-gray-400 mb-10">{{ dataTeam.graduate }}</p>
+    <h3 class="z-20 font-bold -mt-5 px-5 text-white relative">{{ dataTeam.bio }}</h3>
+    <p class="z-20 px-7 pt-3 text-gray-300">{{ dataTeam.text }}</p>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    <Data :data-team="dataDaniel" />
-    <Data :data-team="dataJonathan" />
-    <Data :data-team="dataKira" />
-    <Data :data-team="dataJeanette" />
-    <Data :data-team="dataPatrick" />
-  </div>
+  <div class="flex justify-content item-center min-h-screen bg-gray-800">
+    <div class="grid grid-cols-4 gap-4 py-20 px-52">
+        <svg
+          class="ml-96 absolute"
+          width="120"
+          height="100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M104 102V59.727H84.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42   -9.287 6.125-13.61C95.01 25.86 98.909 22.257 104 19.375V0c-9.758 4.27-17.712 9.874-23.864 16.813-6.151 6.939-10.712 14.545-13.681 22.818C63.485 47.904 62 59.941 62 75.74V102h42zm-62 0V59.727H22.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42-9.287 6.125-13.61C33.01 25.86 36.909 22.257 42 19.375V0c-9.652 4.27-17.58 9.874-23.784 16.813C12.01 23.752 7.424 31.358 4.455 39.631 1.485 47.904 0 59.941 0 75.74V102h42z"
+            fill="#A775F1"
+            fill-rule="nonzero"
+          />
+        </svg>
+      <div class="col-start-1 col-span-2 rounded-xl border-solid border-2 black bg-blue-900">
+          <Data :data-team="dataTeam[0]" />
+        </div>
+        <div class="rounded-xl border-solid border-2 black bg-gray-800">
+          <Data :data-team="dataTeam[1]" />
+        </div>
+        <div class="row-span-2 rounded-xl border-solid border-2 black bg-gray-500">
+          <Data :data-team="dataTeam[2]" />
+        </div>
+        <div class="rounded-xl border-solid border-2 black bg-gray-500">
+          <Data :data-team="dataTeam[3]" />
+        </div>
+        <div class="col-start-2 col-span-2 rounded-xl border-solid border-2 black bg-gray-800">
+          <Data :data-team="dataTeam[4]" />
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -16,40 +40,43 @@ export default {
   },
   data() {
     return {
-      dataDaniel: {
-        nameDaniel: "Daniel Clifford",
-        imgDaniel: "/image-daniel.jpg",
-        graduateDaniel: "Verified Graduate",
-        bioDaniel:
-          "I received a job offer mid-course, and the subjects  I learned were current, if not more so, in the company I joined. I honestly feel I got every pennys worth",
-        textDaniel:
-          "“ I was an EMT for many years before I joined the bootcamp. Ive been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter.  The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, Ive successfully switched careers, working as a Software Engineer at a VR startup. ”",
-        nameJonathan: "Jonathan Walters",
-        imgJonathan: "/image-jonathan.jpg",
-        graduateJonathan: "Verified Graduate",
-        bioJonathan: "The team was very supportive and kept me motivated",
-        textJonathan:
-          "“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer  for a big company. This was one of the best investments Ive made in myself. ”",
-        nameKira: "Kira Whittle",
-        imgKira: "/image-kira.jpg",
-        graduateKira: "Verified Graduate",
-        bioKira: "Such a life-changing experience. Highly recommended!",
-        textKira:
-          "“ Before joining the bootcamp, Ive never written a line of code. I needed structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, Ive often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend!",
-        nameJeanette: "Jeanette Harmon",
-        imgJeanette: "/image-jeanette.jpg",
-        graduateJeanette: "Verified Graduate",
-        bioJeanette: "An overall wonderful and rewarding experience",
-        textJeanette:
-          "“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”",
-        namePatrick: "Patrick Abrams",
-        imgPatrick: "/image-patrick.jpg",
-        graduatePatrick: "Verified Graduate",
-        bioPatrick:
-          "Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.",
-        textPatrick:
-          "“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”",
+      dataTeam:[ 
+      {
+        name: "Daniel Clifford",
+        img: "/image-daniel.jpg",
+        graduate: "Verified Graduate",
+        bio: "I received a job offer mid-course, and the subjects  I learned were current, if not more so, in the company I joined. I honestly feel I got every pennys worth",
+        text: "“ I was an EMT for many years before I joined the bootcamp. Ive been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter.  The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, Ive successfully switched careers, working as a Software Engineer at a VR startup. ”",
       },
+      {
+        name: "Jonathan Walters",
+        img: "/image-jonathan.jpg",
+        graduate: "Verified Graduate",
+        bio: "The team was very supportive and kept me motivated",
+        text: "“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer  for a big company. This was one of the best investments Ive made in myself. ”",
+      },
+      {
+        name: "Kira Whittle",
+        img: "/image-kira.jpg",
+        graduate: "Verified Graduate",
+        bio: "Such a life-changing experience. Highly recommended!",
+        text: "“ Before joining the bootcamp, Ive never written a line of code. I needed structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, Ive often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend!",
+      },
+      {
+        name: "Jeanette Harmon",
+        img: "/image-jeanette.jpg",
+        graduate: "Verified Graduate",
+        bio: "An overall wonderful and rewarding experience",
+        text: "“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”",
+      },
+      {
+        name: "Patrick Abrams",
+        img: "/image-patrick.jpg",
+        graduate: "Verified Graduate",
+        bio: "Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.",
+        text: "“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”",
+      },
+     ],
     };
   },
 };

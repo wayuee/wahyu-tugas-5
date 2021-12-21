5a99a2285761be01f6852447dec77fca0099f93f
wayuee <farhancoc5@gmail.com>
2021-12-21 16:45:25 +0700

    add folder

---
+++ components/DataFromIndex.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="flex justify-content item-center min-h-screen bg-gray-800">
+    <div class="grid grid-cols-4 gap-4 py-20 px-52">
+      <div
+        class="col-start-1 col-span-2 rounded-xl border-solid border-2 black bg-blue-900"
+      >
+        <img
+          class="rounded-full float-left mx-5 mt-5"
+          :src="dataDaniel.img"
+          alt="Daniel"
+        />
+        <h4 class="z-20 font-semibold text-white mt-5">
+          {{ dataDaniel.name }}
+        </h4>
+        <p class="z-20 text-gray-400">{{ dataDaniel.graduate }}</p>
+        <svg
+          class="ml-96 -mt-14"
+          width="120"
+          height="100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M104 102V59.727H84.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42   -9.287 6.125-13.61C95.01 25.86 98.909 22.257 104 19.375V0c-9.758 4.27-17.712 9.874-23.864 16.813-6.151 6.939-10.712 14.545-13.681 22.818C63.485 47.904 62 59.941 62 75.74V102h42zm-62 0V59.727H22.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42-9.287 6.125-13.61C33.01 25.86 36.909 22.257 42 19.375V0c-9.652 4.27-17.58 9.874-23.784 16.813C12.01 23.752 7.424 31.358 4.455 39.631 1.485 47.904 0 59.941 0 75.74V102h42z"
+            fill="#A775F1"
+            fill-rule="nonzero"
+          />
+        </svg>
+        <h3 class="z-20 font-bold -mt-5 px-5 text-white">
+          {{ dataDaniel.bio }}
+        </h3>
+        <p class="z-20 px-7 pt-3 text-gray-300">{{ dataDaniel.text }}</p>
+      </div>
+      <div class="rounded-xl border-solid border-2 black bg-gray-800">
+        <img
+          class="rounded-full float-left mt-5 mx-5"
+          :src="dataJonathan.img"
+          alt="Jonathan"
+        />
+        <h4 class="font-semibold text-white mt-5">{{ dataJonathan.name }}</h4>
+        <p class="text-gray-400 pb-5">{{ dataJonathan.graduate }}</p>
+        <h3 class="font-bold px-3 pb-3 text-white">{{ dataJonathan.bio }}</h3>
+        <p class="px-5 text-gray-300">{{ dataJonathan.text }}</p>
+      </div>
+      <div
+        class="row-span-2 rounded-xl border-solid border-2 black bg-gray-500"
+      >
+        <img
+          class="rounded-full float-left mt-5 mx-5"
+          :src="dataKira.img"
+          alt="Kira"
+        />
+        <h4 class="font-semibold text-white mt-5">{{ dataKira.name }}</h4>
+        <p class="text-gray-400 pb-5">{{ dataKira.graduate }}</p>
+        <h3 class="font-bold px-3 pb-3">{{ dataKira.bio }}</h3>
+        <p class="px-3 pb-3 text-gray-300">{{ dataKira.text }}</p>
+      </div>
+      <div class="rounded-xl border-solid border-2 black bg-gray-500">
+        <img
+          class="rounded-full float-left mt-5 mx-5"
+          :src="dataJeanette.img"
+          alt="jeanette"
+        />
+        <h4 class="font-semibold text-white mt-5">{{ dataJeanette.name }}</h4>
+        <p class="text-gray-400 pb-5">{{ dataJeanette.graduate }}</p>
+        <h3 class="font-bold px-3">{{ dataJeanette.bio }}</h3>
+        <p class="px-5 pt-3 text-gray-300">{{ dataJeanette.text }}</p>
+      </div>
+      <div
+        class="col-start-2 col-span-2 rounded-xl border-solid border-2 black bg-gray-800"
+      >
+        <img
+          class="col-span-2 rounded-full float-left mt-5 mx-5"
+          :src="dataPatrick.img"
+          alt="patrick"
+        />
+        <h4 class="font-semibold text-white mt-5">{{ dataPatrick.name }}</h4>
+        <p class="text-gray-400 pb-5">{{ dataPatrick.graduate }}</p>
+        <h3 class="font-bold px-5 pb-3 text-white">{{ dataPatrick.bio }}</h3>
+        <p class="px-5 text-gray-300">{{ dataPatrick.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dataDaniel: {
+      type: Object,
+      default: {},
+    },
+    dataJonathan: {
+      type: Object,
+      default: {},
+    },
+    dataKira: {
+      type: Object,
+      default: {},
+    },
+    dataJeanette: {
+      type: Object,
+      default: {},
+    },
+    dataPatrick: {
+      type: Object,
+      default: {},
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <Data
+    :data-daniel="dataDaniel"
+    :data-jonathan="dataJonathan"
+    :data-kira="dataKira"
+    :data-jeanette="dataJeanette"
+    :data-patrick="dataPatrick"
+  />
+</template>
+
+<script>
+import Data from "~/components/DataFromIndex";
+export default {
+  components: {
+    Data,
+  },
+  data() {
+    return {
+      dataDaniel: {
+        name: "Daniel Clifford",
+        img: "/image-daniel.jpg",
+        graduate: "Verified Graduate",
+        bio: "I received a job offer mid-course, and the subjects  I learned were current, if not more so, in the company I joined. I honestly feel I got every pennys worth",
+        text: "“ I was an EMT for many years before I joined the bootcamp. Ive been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter.  The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, Ive successfully switched careers, working as a Software Engineer at a VR startup. ”",
+      },
+      dataJonathan: {
+        name: "Jonathan Walters",
+        img: "/image-jonathan.jpg",
+        graduate: "Verified Graduate",
+        bio: "The team was very supportive and kept me motivated",
+        text: "“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer  for a big company. This was one of the best investments Ive made in myself. ”",
+      },
+      dataKira: {
+        name: "Kira Whittle",
+        img: "/image-kira.jpg",
+        graduate: "Verified Graduate",
+        bio: "Such a life-changing experience. Highly recommended!",
+        text: "“ Before joining the bootcamp, Ive never written a line of code. I needed structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, Ive often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend!",
+      },
+      dataJeanette: {
+        name: "Jeanette Harmon",
+        img: "/image-jeanette.jpg",
+        graduate: "Verified Graduate",
+        bio: "An overall wonderful and rewarding experience",
+        text: "“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”",
+      },
+      dataPatrick: {
+        name: "Patrick Abrams",
+        img: "/image-patrick.jpg",
+        graduate: "Verified Graduate",
+        bio: "Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.",
+        text: "“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”",
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
